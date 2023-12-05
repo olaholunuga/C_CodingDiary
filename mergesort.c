24be2b1b41b00a4304f5c689732f3f974f876529
olaholunuga <olaholunuga@gmail.com>
2023-12-05 20:44:55 +0100

    tyr

--- mergesort.c
+++ mergesort.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 
-
+void mergesort(int arr[10]);
+void merge(int arr[10], int start, int end);
 
 void main()
 {
-    mergesort()
+    int arr[10];
+    arr = [7, 6, 9, 5, 1, 3, 2, 4, 0, 8];
+    mergesort(arr);
+}
+
+void mergesort(int arr[10])
+{
+    for(int i = 0; i < 10; i++)
+    {
+        merge(arr, i, i + 1);
+    }
+}
+
+void merge(int arr[10], int start, int end)
+{
+    int i = 0;
+    int temp;
+    while(i <= start)
+    {
+        if(arr[i] > arr[end])
+        {
+            temp = arr[i];
+            arr[i] = arr[end];
+            arr[end] = temp;
+        }
+        i++;
+    }
 }
